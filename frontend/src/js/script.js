e26b3a3ff522c1e4b13c1e93d17d39db2722988f
Joe <javokhir.369@gmail.com>
2024-02-11 23:25:08 +0500

    gallery step 2

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -8,3 +8,14 @@ document.addEventListener('DOMContentLoaded', function () {
         darkModeToggle.classList.toggle('dark-mode');
     });
 });
+
+function openFullScreen(img) {
+    const modal = document.getElementById("myModal");
+    const modalImg = document.getElementById("img01");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+function closeFullScreen() {
+    document.getElementById("myModal").style.display = "none";
+}
