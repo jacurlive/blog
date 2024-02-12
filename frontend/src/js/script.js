7364937b7e3bc16b6a55a76c8843c1fd8d4726bf
Joe <javokhir.369@gmail.com>
2024-02-13 01:03:01 +0500

    13 feb

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -19,3 +19,31 @@ function openFullScreen(img) {
 function closeFullScreen() {
     document.getElementById("myModal").style.display = "none";
 }
+
+const modal = document.getElementById("myModal");
+const modalImg = document.getElementById("img01");
+
+function openFullScreen(img) {
+    modal.style.display = "block";
+    modalImg.src = img.src;
+
+    modal.addEventListener("click", closeFullScreen);
+
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "Escape") {
+            closeFullScreen();
+        }
+    });
+}
+
+function closeFullScreen() {
+    modal.style.display = "none";
+
+    modal.removeEventListener("click", closeFullScreen);
+
+    document.removeEventListener("keydown", function(event) {
+        if (event.key === "Escape") {
+            closeFullScreen();
+        }
+    });
+}
