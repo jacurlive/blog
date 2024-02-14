639df672fdd27bfbfbea694db4643dd7a23cb38b
jacurlive <jasurkkirchh@gmail.com>
2024-02-14 11:21:53 +0500

    add blog, about to admin and add main view

--- backend/articles/admin.py
+++ backend/articles/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import About, Blog, ImagePost
+
+
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    fields = ("full_name", "specialization", "image", "instagram_link", "github_link", "linkedin_link", "telegram_link", "short_bio", "bio")
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    fields = ("title", "content")
+
+
+@admin.register(ImagePost)
+class ImagePostAdmin(admin.ModelAdmin):
+    fields = ("image", "content")
--- backend/articles/views.py
+++ backend/articles/views.py
@@ -1,3 +1,23 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import About, ImagePost
 
-# Create your views here.
+
+
+class MainView(ListView):
+    # queryset = About.objects.first()
+    template_name = "blog/index.html"
+    context_object_name = "about"
+
+    def get_queryset(self):
+        about = About.objects.first()
+        print(about)
+        # image_post = ImagePost.objects.all()
+        context = {
+            "about": about,
+            # "image": image_post
+        }
+        print(context)
+        return context
